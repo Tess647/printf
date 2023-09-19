3502cbb6551e686578ffb1a1d7cf6f6ecd3e491d
Tess647 <thanni_taslihah@yahoo.com>
2023-09-19 01:36:33 -0700

    file modified

--- printf_binary.c
+++ printf_binary.c
@@ -9,17 +9,17 @@ int printf_bin(va_list list_args)
 {
 	unsigned int n = va_arg(list_args, unsigned int);
 	unsigned int j;
-	int charCount = 0, b = 1, z, i, num;
+	int charCount = 0, b = 1, z, i, num = 0;
 
 	for (i = 0; i < 32; i++)
 	{
-		j = ((b << (31 - i)) & n); 
+		j = ((b << (31 - i)) & n);
 	/* set a single bit at position corresponding to current iteration */
 	/* and isolates the bit at the current position of interest in n */
 
 		if (j >> (31 - i))
 			num = 1;/* if statement to remove leading zeros */
-		
+
 		if (num)
 		{
 			z = j >> (31 - i);
