90329eac764caf0783c140652d75d167d4abb7c6
Tess647 <thanni_taslihah@yahoo.com>
2023-09-19 12:59:11 -0700

    file modified

--- printf_hex.c
+++ printf_hex.c
@@ -28,7 +28,7 @@ int printf_hex(va_list list_args)
 	for (i = count - 1; i >= 0; i--)
 	{
 		if (hexArr[i] > 9)
-			hexArr[i] = hexArr[i] + 39;
+			hexArr[i] = hexArr[i] + 55;
 		_putchar(hexArr[i] + '0');
 	}
 
