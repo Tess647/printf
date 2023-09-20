c9b78764d0b31c3f2bbebe6bdcef18c084453401
Tess647 <thanni_taslihah@yahoo.com>
2023-09-19 17:17:46 -0700

    file

--- printf_signedint.c
+++ printf_signedint.c
@@ -1,43 +1,45 @@
-#include <stdlib.h>
 #include "main.h"
-#include <unistd.h>
 
 /**
-  *printf_signedint - print a signed int
-  *@list_args: input argument
-  *Return: print count
+  * printf_signednt - prints integer
+  * @list_args: input argument
+  * Return: number of characters printed
   */
 int printf_signedint(va_list list_args)
 {
-	signed int num = va_arg(list_args, signed int);
-	int temp = num, i;
-	int numofdigits = 1;
-	int charcount = 0;
-	char *str, m = '-';
+	int p = va_arg(list_args, int);
+	int num, last_digit = p % 10, digit;
+	int charCount = 1, e = 1;
 
-	while (temp /= 10)
-		numofdigits++;
+	p = p / 10;
+	num = p;
 
-	str = malloc(numofdigits + 1);
-
-	if (!str)
-		return (-1);
-
-	str[numofdigits] = '\0';
-	if (num < 0)
+	if (last_digit < 0)
 	{
-		write(1, &m, 1);
+		_putchar('-');
 		num = -num;
-		charcount++;
+		last_digit = -last_digit;
+		charCount++;
 	}
-
-	for (i = numofdigits; i >= 0; i--)
+	if (num > 0)
 	{
-		str[i] = '0' + (num % 10);
-		num /= 10;
+		while (num / 10 != 0)
+		{
+			e *= 10;
+			num /= 10;
+		}
+		num = p;
+
+		while (e > 0)
+		{
+			digit = num / e;
+			_putchar(digit + '0');
+			num = num - (digit * e);
+			e = e / 10;
+			charCount++;
+		}
 	}
-	charcount += numofdigits;
-	write(1, str, numofdigits);
-	free(str);
-	return (charcount);
+	_putchar(last_digit + '0');
+
+	return (charCount);
 }
